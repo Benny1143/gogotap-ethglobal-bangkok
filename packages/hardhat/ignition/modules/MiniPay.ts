955906f817314adb749150fe6a175f34ffc9f9a2
Benny Goh <bennyrobot1143@gmail.com>
2024-11-16 20:51:23 +0700

    update initialOwner to test address

--- packages/hardhat/ignition/modules/MiniPay.ts
+++ packages/hardhat/ignition/modules/MiniPay.ts
@@ -4,7 +4,7 @@ const MiniPayModule = buildModule("MiniPayModule", (m) => {
   // Set up parameters if you want to make the contract address configurable
   const initialOwner = m.getParameter(
     "initialOwner",
-    "0x1724707c52de2fa65ad9c586b5d38507f52D3c06"
+    "0xD7a5b3546fd19cbF989666203D5296000Cad7888"
   );
 
   // Deploy the MiniPay contract with the specified parameters
